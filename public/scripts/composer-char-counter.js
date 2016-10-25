a9900ebcaa0a07f5131d530861404edeaccaa589
Noam Ascher <noam.ascher@gmail.com>
2016-10-25 01:32:51 +0000

    Formatted for submission - a few extra touches

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,10 +1,7 @@
 $('document').ready(function() {
   const initColor = $('.counter').css("color");
   $('textarea').on('keyup', function() {
-    //console.log($(this)['context'].selectionEnd);
-    //console.log($(this));
     var count = $(this).val().length;
-    //console.log(count);
     var counter = $(this).parent().children('.counter');
     counter.text(140 - count);
     if (count > 140) {
